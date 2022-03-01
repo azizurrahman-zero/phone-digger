7237b096dabf999ee35130730d443533c5b52269
azizurrahman-zero <azizurrahman.zero@gmail.com>
2022-03-01 14:56:01 +0600

    error fixed

--- js/app.js
+++ js/app.js
@@ -17,8 +17,14 @@ const searchMobile = () => {
     const searchBox = document.getElementById('search-box');
     const searchText = searchBox.value;
 
-    // empty search box
+    // clear search box
     searchBox.value = '';
+
+    // clear result container
+    resultContainer.textContent = '';
+
+    // hide more details
+    mobileDetails.style.display = 'none';
 
     // check error case
     if (searchText == '') {
@@ -42,10 +48,10 @@ const searchMobile = () => {
 const displayResult = result => {
     const dataStatus = result.status;
     const mobiles = result.data;
+    const mobilesSliced = mobiles.slice(0, 20);
     if (dataStatus == true) {
         resultContainer.style.display = 'flex';
-        mobiles.forEach(mobile => {
-
+        mobilesSliced.forEach(mobile => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -57,8 +63,8 @@ const displayResult = result => {
                         <h5 class="card-title">${mobile.phone_name}</h5>
                         <p class="card-text">by ${mobile.brand}</p>
                     </div>
-                    <div class="card-footer green-bg">
-                        <button onclick="loadDetails('${mobile.slug}')" class="btn btn-light px-4 py-1 rounded-3 font-roboto fw-medium">Details</button>
+                    <div class="card-footer bg-white border-top-0">
+                        <button onclick="loadDetails('${mobile.slug}')" class="btn px-4 py-1 rounded-3 blue-bg text-white font-roboto fw-medium">Details</button>
                     </div>
                 </div>
             `
@@ -75,6 +81,7 @@ const displayResult = result => {
 
 // load mobile details function
 const loadDetails = phoneId => {
+    window.location.href = '#mobile-details';
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(res => res.json())
@@ -86,20 +93,69 @@ const loadDetails = phoneId => {
 
 // display mobile details function
 const displayDetails = mobile => {
-
+    const { mainFeatures, others } = mobile;
+    console.log(mainFeatures);
     mobileDetails.style.display = 'block';
     mobileImage.src = `${mobile.image}`
     mobileName.innerText = `${mobile.name}`;
     detailsText.innerHTML = `
         <ul>
-            <li>Release Date : ${mobile.releaseDate ? mobile.releaseDate : 'Unknown'}</li>
-            <li>Features : 
-                <ul>
-                    <li>Chipset : ${mobile.mainFeatures.chipSet}</li>
-                    <li>Display Size : ${mobile.mainFeatures.displaySize}</li>
-                    <li>Memory : ${mobile.mainFeatures.memory}</li>
-                    <li>Storage : ${mobile.mainFeatures.storage}</li>
-                </ul>
+            <li class="fs-5">Release Date : ${mobile.releaseDate ? mobile.releaseDate : 'Unknown'}</li>
+            <li class="fs-5">Main Features : 
+                <table class="table caption-top fs-6">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Chipset</th>
+                            <td>${mainFeatures?.chipSet ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Display Size</th>
+                            <td>${mainFeatures?.displaySize ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Memory</th>
+                            <td>${mainFeatures?.memory ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Storage</th>
+                            <td>${mainFeatures?.storage ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Sensor</th>
+                            <td>${mainFeatures?.sensors ?? 'Unknown'}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </li>
+            <li class="fs-5">Other Features :
+                <table class="table caption-top fs-6">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Bluetooth</th>
+                            <td>${others?.Bluetooth ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">GPS</th>
+                            <td>${others?.GPS ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">NFC</th>
+                            <td>${others?.NFC ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Radio</th>
+                            <td>${others?.Radio ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">USB</th>
+                            <td>${others?.USB ?? 'Unknown'}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">WLAN</th>
+                            <td>${others?.WLAN ?? 'Unknown'}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </li>
         </ul>
     `
